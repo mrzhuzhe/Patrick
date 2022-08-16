7c2ded0cc46059fe88500be59c9d80e54d43abb7
mrzhuzhe <466648706@qq.com>
2022-08-16 15:07:11 +0800

    feature(RingNormal): tools for UR5 calculate rim normal for convexhull

--- geo/contour/calculate_normal.py
+++ geo/contour/calculate_normal.py
@@ -6,29 +6,13 @@ from find_convexhull import fine_one_demension_convexhull
 ascii_grid = np.loadtxt("ply/rd9c37-lq-Points-1mm.asc", skiprows=1)
 
 #   only need one side  for better plot
-ascii_grid = ascii_grid[ascii_grid[:,2]>=0]
-
-
-fig = plt.figure(figsize=(8, 8))
-ax = fig.add_subplot()
-
-x = ascii_grid[:, 0]
-y = ascii_grid[:, 2]
-# z = 0
-
-# for better plot rotate 90 degree
-ax.scatter(y, x)
-ax.plot(y, x, color="green")
-
-
-ascii_grid = fine_one_demension_convexhull(ascii_grid)
-
+ascii_grid = ascii_grid[ascii_grid[:,0]>=0]
 
 #ascii_grid = ascii_grid[-10:]
 
 """
 fig = plt.figure()
-ax = fig.add_subplot("3d", projection='3d')
+ax = fig.add_subplot(111, projection='3d')
 ax.scatter(ascii_grid[:,0], ascii_grid[:,1], ascii_grid[:,2])
 """
 
@@ -36,46 +20,68 @@ fig = plt.figure(figsize=(8, 8))
 ax = fig.add_subplot()
 
 x = ascii_grid[:, 0]
-y = ascii_grid[:, 2]
-# z = 0
+# y = 0
+z = ascii_grid[:, 2]
 
 # for better plot rotate 90 degree
-ax.scatter(y, x)
-ax.plot(y, x, color="green")
+ax.scatter(x, z)
+ax.plot(x, z, color="green")
 
 
 
-# this seems still need to calculate contour for make sure get the most out side contour
+
+# this seems still need to calculate convex contour for make sure get the most out side contour
+one_demension_convexhull, _min, _max = fine_one_demension_convexhull(ascii_grid)
+print("_min, _max ", _min, _max)
+ch_x = one_demension_convexhull[:, 0]
+ch_z = one_demension_convexhull[:, 2]
+
+# TCP length is 15mm
+_TCPLengh_ = 150
+ 
 i = 1
-middlepointX = []
-middlepointY = []
-middlepointNoral = []
-while i<len(x):
-    _y = (y[i]+y[i-1])/2
-    _x = (x[i]+x[i-1])/2
+middlepointX = []   
+middlepointZ = []   
+middlepointNoralDegree = []  # 注意：此处为仰角(俯视为负数)
+TCPpositionX = []
+TCPpositionZ = []
+
+# only loop points convexhull
+while i<len(ch_x):
+    
+    _x = (ch_x[i]+ch_x[i-1])/2
+    _z = (ch_z[i]+ch_z[i-1])/2
+
     middlepointX.append(_x)
-    middlepointY.append(_y)
-    norm = np.sqrt((x[i]-x[i-1])* (x[i]-x[i-1]) + (y[i]-y[i-1]) *(y[i]-y[i-1]))
+    middlepointZ.append(_z)
+    norm = np.sqrt(np.power(ch_x[i]-ch_x[i-1], 2) + np.power(ch_z[i]-ch_z[i-1], 2))
 
     if float(norm) == 0.0:
         norm = 1
 
-    #"""
-    degree = 90
-    #if y[i]-y[i-1] == 0:
-    if x[i]-x[i-1] == 0:
+    degree = 0
+    if ch_x[i]-ch_x[i-1] == 0:
         pass
     else:
-        #degree = int(np.arctan(- (x[i]-x[i-1]) / (y[i]-y[i-1]))* 180 / np.pi)
-        degree = int(np.arctan(- (y[i]-y[i-1]) / (x[i]-x[i-1]))* 180 / np.pi)
-        middlepointNoral.append(degree)
-    if degree < 0:
-        ax.text(0.1 + _y, _x, degree, fontsize=12)
-    #"""
+        degree = np.arctan(- (ch_x[i]-ch_x[i-1]) / (ch_z[i]-ch_z[i-1]))* 180 / np.pi
 
-        ax.plot([_y, _y +  (x[i]-x[i-1])/ norm], [_x, _x - (y[i]-y[i-1])/ norm], color="black")
+    if degree < 0:
+        _TCP_X = _x - (ch_z[i]-ch_z[i-1])/ norm * _TCPLengh_
+        _TCP_Z = _z +  (ch_x[i]-ch_x[i-1])/ norm * _TCPLengh_
+        TCPpositionX.append(_TCP_X)
+        TCPpositionZ.append(_TCP_Z)
+        middlepointNoralDegree.append(degree)
+
+        ax.text(_x, 0.1 + _z, "x:" + str(int(_x)) + " z:" +  str(int(_z)) , fontsize=12)
+        ax.text(_TCP_X, _TCP_Z, "x:" + str(int(_TCP_X)) + " z:" +  str(int(_TCP_Z)) + " deg:" + str(int(degree)), fontsize=12)
+        #print("TCP Norm", np.dot([_TCP_X-_x, _TCP_Z-_z], [_TCP_X-_x, _TCP_Z - _z]))    
+        ax.plot([_x, _TCP_X], [_z, _TCP_Z], color="black")
     i+=1
 
-ax.scatter(middlepointY, middlepointX, color="red")
+ax.scatter(middlepointX, middlepointZ, color="red")
+
+print("len(TCPpositionX), len(TCPpositionZ), len(middlepointNoralDegree)", len(TCPpositionX), len(TCPpositionZ), len(middlepointNoralDegree))
+# absolute value for Z axie
+print(TCPpositionX, TCPpositionZ - _min, middlepointNoralDegree)
 
 plt.show()
--- geo/contour/find_convexhull.py
+++ geo/contour/find_convexhull.py
@@ -1,22 +1,23 @@
 # 2 demensional convexhull example
 # https://github.com/mrzhuzhe/vanilla/blob/master/geometry/convexhull.jsimport 
+# for simplify only use 1D
 
 import numpy as np
 
 def fine_one_demension_convexhull(data):
     # sort by X axies
-    data = sorted(data, key=lambda x: x[2])
+    data = sorted(data, key=lambda x: x[0])
 
     # only keep element only when it is maximal an minimal on Y axies 
     _res = []
-    _max = _min = data[0][0]
+    _max = _min = data[0][2]
     for i in data:
-        _cur = i[0]
+        _cur = i[2]
         if _cur > _max:
             _max = _cur
             _res.append(i)
-        elif i[0] < _min:
+        elif _cur < _min:
             _min = _cur
             _res.append(i)
-    _res = sorted(_res, key=lambda x: x[0])
-    return np.array(_res)
+    _res = sorted(_res, key=lambda x: x[2])
+    return np.array(_res), _min, _max
